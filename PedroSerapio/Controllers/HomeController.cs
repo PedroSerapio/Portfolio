37314dcede827d908ea7d6540258e62ad21122f7
serap <serap@SERAPIO>
2023-10-20 00:18:18 +0100

    Add project files.

---
+++ PedroSerapio/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PedroSerapio.Models;
+using System.Diagnostics;
+
+namespace PedroSerapio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            HomeModel home = new HomeModel();
+
+            home.Nome = "Pedro";
+            home.Email = "pedro@teste.pt";
+
+            return View(home);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+
+    public class DownloadController : Controller
+    {
+        public IActionResult DownloadCV()
+        {
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Downloads/PedroSerápio_en.pdf");
+
+            return PhysicalFile(filePath, "application/pdf", "PedroSerápio_en.pdf");
+        }
+    }
+}
---
+++ PedroSerapio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PedroSerapio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
